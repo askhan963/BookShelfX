2f0711d84df8d7fc6e9fd508ec4369f60b931614
askhan963 <awaisknas963@gmail.com>
2024-10-19 15:55:40 +0500

    Added NavBar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom"
+import './App.css'
+import Navbar from "./components/Navbar"
 export default function App() {
   return (
     <>
-    <header>Header</header>
-    <main className="min-h-screen max-w-screen-2xl mx-auto px-4 py-6">
+    <Navbar/>
+    <main className="min-h-screen max-w-screen-2xl mx-auto px-4 py-6 font-primary">
     <Outlet />
     </main>
     <footer>Footer</footer>
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,62 @@
+import {useState} from 'react'
+import { HiMiniBars3CenterLeft, HiOutlineShoppingCart} from 'react-icons/hi2'
+import {IoSearchOutline } from 'react-icons/io5'
+import { HiOutlineUser , HiOutlineHeart } from 'react-icons/hi2'
+import { Link } from 'react-router-dom'
+import AvatarImg from '../assets/avatar.png'
+const Navbar = () => {
+    const currentUser = true;
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  return (
+    <header className='max-w-screen-2xl mx-auto px-4 py-6'>
+      <nav className='flex justify-between items-center'>
+        {/* Left */}
+        <div className='flex items-center md:gap-16 gap-4 '>
+            <Link to='/'>
+            <HiMiniBars3CenterLeft className='size-8' /></Link>
+            <div className='relative sm:w-72 w-40  space-x-2 '>
+            <IoSearchOutline 
+            className='absolute inline-block left-3 inset-y-2' />
+            <input type="text" placeholder='Search' 
+            className='bg-[#EAEAEA] w-full py-1 md:px-8 px-6 rounded-md focus:outline-none' 
+            />
+            </div>
+        </div>
+        <div>
+           
+        </div>
+        {/* Right */}
+        <div className='relative flex items-center md:space-x-3 space-x-2'>
+            {
+                currentUser ? (
+                   <>
+                   <button onClick={()=>setIsDropdownOpen(!isDropdownOpen)}>
+                    <img src={AvatarImg} alt="Avatar Image "
+                    className={`size-7 rounded-full ${
+                        currentUser    ? 'ring-2 ring-blue-500' : '' 
+                    }`}
+                    />
+                   </button>
+                   </>
+                ) : (
+                    <Link to={'/login'}>
+                    <HiOutlineUser className='size-8' /></Link>
+                )
+            }
+            
+            <button className='hidden sm:block '>
+                <HiOutlineHeart className='size-8' />
+            </button>
+            <Link to='/cart' className='bg-primary p-1 sm:px-6 px-2 flex items-center rounded-md '>
+            
+                <HiOutlineShoppingCart className='size-8' />
+          <span className='text-sm font-semibold sm:ml-1'>0</span>
+            </Link>
+        </div>
+      </nav>
+          </header>
+  )
+
+  }
+
+export default Navbar
