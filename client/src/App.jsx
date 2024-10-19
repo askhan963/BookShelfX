bb0268358b56cadb2235136412ab98f3a8be3981
askhan963 <awaisknas963@gmail.com>
2024-10-19 14:43:25 +0500

    React-Router-Dom Setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,11 @@ import { Outlet } from "react-router-dom"
 export default function App() {
   return (
     <>
+    <header>Header</header>
+    <main className="min-h-screen max-w-screen-2xl mx-auto px-4 py-6">
     <Outlet />
+    </main>
+    <footer>Footer</footer>
       </>
   )
 }
--- client/src/routers/router.jsx
+++ client/src/routers/router.jsx
@@ -8,7 +8,19 @@ const router = createBrowserRouter([
             {
                 path: '/',
                 element: <h1>Hello World</h1>
-            }
+            },
+            {
+                path: '/about',
+                element: <h1>About</h1>
+            },
+            {
+                path: '/contact',
+                element: <h1>Contact</h1>
+            },
+            {
+                path: '/orders',
+                element: <h1>Orders</h1>
+            },
         ]
     },
 ]);
