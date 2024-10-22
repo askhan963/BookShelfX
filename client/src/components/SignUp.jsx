7f2e4eb635f077450a571301391d7927b0ce7cdf
askhan963 <awaisknas963@gmail.com>
2024-10-22 11:15:39 +0500

    Add hooks form to LogIn and SignUp

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,31 +1,37 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { FaGoogle } from "react-icons/fa";
+import { useForm } from "react-hook-form"
 
-
-const SingUp = () => {
-    
-   
+const SignUp = () => {
+    const [message, setMessage] = useState("");
+    // console.log(registerUser)
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+      } = useForm()
 
     //   register user
 
       const onSubmit = async(data) => {
-        // console.log(data)
-      
+        
       }
 
       const handleGoogleSignIn = async() => {
-        
+       
       }
   return (
     <div className='h-[calc(100vh-120px)] flex justify-center items-center '>
     <div className='w-full max-w-sm mx-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'>
         <h2 className='text-xl font-semibold mb-4'>Please Register</h2>
 
-        <form onSubmit={onSubmit}>
+        <form onSubmit={handleSubmit(onSubmit)}>
             <div className='mb-4'>
                 <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor="email">Email</label>
                 <input 
+                {...register("email", { required: true })} 
                 type="email" name="email" id="email" placeholder='Email Address'
                 className='shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow'
                 />
@@ -33,11 +39,14 @@ const SingUp = () => {
             <div className='mb-4'>
                 <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor="password">Password</label>
                 <input 
+                {...register("password", { required: true })} 
                 type="password" name="password" id="password" placeholder='Password'
                 className='shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow'
                 />
             </div>
-           
+            {
+                message && <p className='text-red-500 text-xs italic mb-3'>{message}</p>
+            }
             <div>
                 <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded focus:outline-none'>Register</button>
             </div>
@@ -60,4 +69,4 @@ const SingUp = () => {
   )
 }
 
-export default SingUp
+export default SignUp
--- client/src/components/Singin.jsx
+++ client/src/components/Singin.jsx
@@ -1,15 +1,20 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { FaGoogle } from "react-icons/fa";
-
+import { useForm } from "react-hook-form"
 
 const Singin = () => {
     const [message, setMessage] = useState("")
     const navigate = useNavigate()
-   
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+      } = useForm()
 
       const onSubmit = async (data) => {
-        
+       
       }
 
       const handleGoogleSignIn = async () => {
@@ -20,11 +25,11 @@ const Singin = () => {
         <div className='w-full max-w-sm mx-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'>
             <h2 className='text-xl font-semibold mb-4'>Please Login</h2>
 
-            <form onSubmit={onSubmit}>
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <div className='mb-4'>
                     <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor="email">Email</label>
                     <input 
-            
+                    {...register("email", { required: true })} 
                     type="email" name="email" id="email" placeholder='Email Address'
                     className='shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow'
                     />
@@ -32,10 +37,14 @@ const Singin = () => {
                 <div className='mb-4'>
                     <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor="password">Password</label>
                     <input 
+                    {...register("password", { required: true })} 
                     type="password" name="password" id="password" placeholder='Password'
                     className='shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow'
                     />
                 </div>
+                {
+                    message && <p className='text-red-500 text-xs italic mb-3'>{message}</p>
+                }
                 <div>
                     <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded focus:outline-none'>Login </button>
                 </div>
