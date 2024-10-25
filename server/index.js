941d3c48577ecd8465842ff980e2d4cd809429c1
askhan963 <awaisknas963@gmail.com>
2024-10-25 09:48:36 +0500

    Creating Server

---
+++ server/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import env from 'dotenv';
+env.config();
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
